dd0a1017d5f73667a78cc3fe1906c71da8a36205
David Hjelm <>
2022-12-02 11:08:43 +0100

    d02p02

--- src/d02.rs
+++ src/d02.rs
@@ -1,6 +1,6 @@
 use std::{fs, io};
 
-#[derive(Debug, PartialEq)]
+#[derive(Debug, PartialEq, Clone, Copy)]
 enum Hand {
     Rock,
     Paper,
@@ -34,6 +34,26 @@ fn is_win(opponent: Hand, own: Hand) -> Outcome {
     }
 }
 
+fn hand_matching_outcome(opponent: Hand, outcome: Outcome) -> Hand {
+    match opponent {
+        Hand::Rock => match outcome {
+            Outcome::Win => Hand::Paper,
+            Outcome::Draw => Hand::Rock,
+            Outcome::Loss => Hand::Scissors,
+        },
+        Hand::Paper => match outcome {
+            Outcome::Draw => Hand::Paper,
+            Outcome::Loss => Hand::Rock,
+            Outcome::Win => Hand::Scissors,
+        },
+        Hand::Scissors => match outcome {
+            Outcome::Draw => Hand::Scissors,
+            Outcome::Win => Hand::Rock,
+            Outcome::Loss => Hand::Paper,
+        },
+    }
+}
+
 fn count_score(opponent: Hand, own: Hand) -> i32 {
     let mut sum = 0;
 
@@ -52,7 +72,7 @@ fn count_score(opponent: Hand, own: Hand) -> i32 {
     sum
 }
 
-fn to_hand(input: char) -> Hand {
+fn char_to_hand(input: char) -> Hand {
     match input {
         'A' | 'X' => Hand::Rock,
         'B' | 'Y' => Hand::Paper,
@@ -61,24 +81,46 @@ fn to_hand(input: char) -> Hand {
     }
 }
 
-fn string_to_hands(input: &str) -> (Hand, Hand) {
-    let hands: Vec<&str> = input.split(' ').collect();
-    let opp_hand: char = hands[0].chars().last().unwrap();
-    let own_hand: char = hands[1].chars().last().unwrap();
+fn char_to_outcome(input: char) -> Outcome {
+    match input {
+        'X' => Outcome::Loss,
+        'Y' => Outcome::Draw,
+        'Z' => Outcome::Win,
+        _ => panic!(),
+    }
+}
 
-    (to_hand(opp_hand), to_hand(own_hand))
+fn string_to_hands(input: &str) -> (Hand, Hand) {
+    let inputs: Vec<&str> = input.split(' ').collect();
+    let opp_hand: char = inputs[0].chars().last().unwrap();
+    let own_hand: char = inputs[1].chars().last().unwrap();
+
+    (char_to_hand(opp_hand), char_to_hand(own_hand))
+}
+
+fn string_to_hand_and_outcome(input: &str) -> (Hand, Outcome) {
+    let inputs: Vec<&str> = input.split(' ').collect();
+    let opp_hand: char = inputs[0].chars().last().unwrap();
+    let wanted_outcome: char = inputs[1].chars().last().unwrap();
+
+    (char_to_hand(opp_hand), char_to_outcome(wanted_outcome))
 }
 
 fn main() -> io::Result<()> {
     let data = fs::read_to_string("./inputs/d02").expect("Should open");
 
-    let mut sum = 0;
+    let mut sum_pt1 = 0;
+    let mut sum_pt2 = 0;
+
     for line in data.lines() {
         let hands = string_to_hands(line);
-        sum += count_score(hands.0, hands.1);
+        let hand_and_outcome = string_to_hand_and_outcome(line);
+        let outcome = hand_matching_outcome(hand_and_outcome.0, hand_and_outcome.1);
+        sum_pt1 += count_score(hands.0, hands.1);
+        sum_pt2 += count_score(hands.0, outcome)
     }
 
-    println!("Total score: {}", sum);
+    println!("Total score Part 1: {}, Part 2: {}", sum_pt1, sum_pt2);
 
     Ok(())
 }
@@ -94,4 +136,31 @@ fn tfunc() {
     assert_eq!(is_win(Hand::Rock, Hand::Paper), Outcome::Win);
 
     assert_eq!(count_score(Hand::Rock, Hand::Paper), 8);
+
+    assert_eq!(
+        string_to_hand_and_outcome(input[0]),
+        (Hand::Rock, Outcome::Draw)
+    );
+
+    assert_eq!(
+        string_to_hand_and_outcome(input[1]),
+        (Hand::Paper, Outcome::Loss)
+    );
+
+    assert_eq!(
+        string_to_hand_and_outcome(input[2]),
+        (Hand::Scissors, Outcome::Win)
+    );
+
+    assert_eq!(hand_matching_outcome(Hand::Rock, Outcome::Draw), Hand::Rock);
+    assert_eq!(
+        hand_matching_outcome(Hand::Paper, Outcome::Loss),
+        Hand::Rock
+    );
+    assert_eq!(
+        hand_matching_outcome(Hand::Scissors, Outcome::Win),
+        Hand::Rock
+    );
+
+    assert_eq!(count_score(Hand::Rock, Hand::Rock), 4);
 }
